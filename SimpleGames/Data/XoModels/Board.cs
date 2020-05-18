47d6c5b81e6c8fa669f08f726c0fa60247a71d55
Casey Asher <casey.e.asher@outlook.com>
2020-05-17 22:31:48 -0500

    up and running

---
+++ SimpleGames/Data/XoModels/Board.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SimpleGames.Data
+{
+    public class Cell
+    {
+        public string Symbol { get; set; } = "_";
+        public string Location { get; set; }
+
+    }
+
+    public class Board
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool XTurn { get; set; } = true;
+        public Cell [,] Grid { get; set; }
+
+        public Board()
+        {
+
+        }
+        public Board(Board board)
+        {
+            Width = board.Width;
+            Height = board.Height;
+            Grid = board.Grid;
+        }
+        public Board(int size)
+        {
+            Width = size;
+            Height = size;
+            InitializeBoardValues(size);
+        }
+
+    public void InitializeBoardValues(int size)
+        {
+            Grid = new Cell[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Grid[i, j] = new Cell();
+                    Grid[i, j].Location = String.Format("{0}{1}", i, j);
+                }
+            }
+        }
+    }
+}
