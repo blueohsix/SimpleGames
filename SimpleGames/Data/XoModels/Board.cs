2dd20cfe6ebd92542a93c253da9dbf7dbd32fd3b
Casey Asher <casey.e.asher@outlook.com>
2020-05-21 20:01:24 -0500

    Move, ValidateMove, and Reset methods removed from TicTacToe and placed in Board. Much cleaner. Yay OOP!

--- SimpleGames/Data/XoModels/Board.cs
+++ SimpleGames/Data/XoModels/Board.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace SimpleGames.Data
 {
@@ -11,10 +12,10 @@ namespace SimpleGames.Data
 
     public class Board
     {
-        public int Size { get; set; }
-        public bool XTurn { get; set; } = true;
-        public string XorO { get; set; } = "X";
-        public Cell [,] Grid { get; set; }
+        public int Size { get; private set; }
+        public bool XTurn { get; private set; } = true;
+        public string XorO { get; private set; } = "X";
+        public Cell[,] Grid { get; private set; }
 
         public Board()
         {
@@ -31,7 +32,7 @@ namespace SimpleGames.Data
             InitializeBoardValues(size);
         }
 
-    public void InitializeBoardValues(int size)
+        private void InitializeBoardValues(int size)
         {
             Grid = new Cell[size, size];
             for (int i = 0; i < size; i++)
@@ -42,6 +43,46 @@ namespace SimpleGames.Data
                     Grid[i, j].Location = String.Format("{0}{1}", i, j);
                 }
             }
+        }
+
+        public void Move(MouseEventArgs e, string location, Board board)
+        {
+            var first = int.Parse(location[0].ToString());
+            var second = int.Parse(location[1].ToString());
+            Console.WriteLine("first = " + first);
+            Console.WriteLine("second = " + second);
+            var cell = board.Grid[first, second];
+            if (ValidateMove(first, second, board))
+            {
+                cell.Symbol = board.XorO;
+                Console.WriteLine("new value = " + cell.Symbol);
+                board.XTurn = !board.XTurn;
+                board.XorO = board.XTurn ? "X" : "O";
+
+            }
+        }
+
+        private bool ValidateMove(int first, int second, Board board)
+        {
+
+            int size = (int)Math.Sqrt(board.Grid.Length);
+            if ((first >= 0 && first <= size - 1) && (second >= 0 && second <= size - 1))
+            {
+                if (board.Grid[first, second].Symbol.Equals("_") &&
+                    !board.Grid[first, second].Symbol.Equals("X") &&
+                    !board.Grid[first, second].Symbol.Equals("O"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ResetBoard(Board board)
+        {
+            board.InitializeBoardValues(board.Size);
+            board.XTurn = true;
+            board.XorO = "X";
         }
     }
 }
